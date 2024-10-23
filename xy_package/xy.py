ebafee8270e094deca787a7ad82dec03ec4e696b
Shashank19git <shashank1992001@gmail.com>
2024-10-24 00:04:15 +0530

    Update xy.py

--- xy_package/xy.py
+++ xy_package/xy.py
@@ -3,7 +3,7 @@ from phyLattice.xy_package.utils import np
 from phyLattice.xy_package.utils import plt
 from torch import randn, pi, sum, cos, roll, exp
 from phyLattice.xy_package.MC_updates import monte_carlo_step
-from phyLattice.xy_package.observables import compute_energy , correlation
+from phyLattice.xy_package.observables import compute_energy, correlation, specific_heat
 
 
 def energy(L ,T, J, n_steps, plot=False, MC_step_size=1):
@@ -12,7 +12,7 @@ def energy(L ,T, J, n_steps, plot=False, MC_step_size=1):
     for step in range(n_steps):
         #if step%100==0:
         #    print(step)
-        spins = monte_carlo_step(spins,L, T, J, MC_step_size)# Collect data
+        spins = monte_carlo_step(spins, L, T, J, MC_step_size)# Collect data
         #print(compute_energy(spins, L, T, J))
         energies.append(compute_energy(spins, L, T, J))
     if plot:
@@ -25,7 +25,16 @@ def energy(L ,T, J, n_steps, plot=False, MC_step_size=1):
         plt.title('Energy vs MC_time')
         plt.show()
     return compute_energy(spins, L, T, J)
-    
+def specific_heat(L, T, J, n_steps, MC_step_size=1):
+    spins = 2 * pi * randn(L, L) - pi
+    energies = []
+    for step in range(n_steps):
+        spins = monte_carlo_step(spins, L, T, J, MC_step_size)
+        energies.append(compute_energy(spins, L, T, J))
+    E_mean = np.mean(energies)
+    E_squared_mean = np.mean(np.array(energies)**2)
+    C_v = (E_squared_mean - E_mean**2) / (T**2)
+    return C_v
 
 def autocorrelation(L, T, J, n_steps, lag, MC_step_size=1):
     spins = 2 * pi * randn(L, L) - pi
